22afd79da8ca656962334ca9cc683d60a9673a33
mrDani <darnnielnology@gmail.com>
2025-02-26 09:53:01 -0600

    Add show action and view for Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "products/index"
   resources :products, only: [:index]
+  resources :products, only: [:index, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
